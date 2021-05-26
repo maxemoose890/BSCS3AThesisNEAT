cb61fd54ec5bbb9165354adf8d010b9b1d7e721b
Bryan Abesamis <40335206+FrozenSonar@users.noreply.github.com>
2021-05-26 22:06:47 +0800

    Add files via upload

--- Fencing.py
+++ Fencing.py
@@ -53,8 +53,8 @@ class Fencer(pygame.sprite.Sprite):
         self.img_count = 0
         self.images = []
         self.images.append(fencer_image)
-        self.img = self.images[0]
-        self.rect = self.img.get_rect()
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
         
         #170,730
         self.pos = vec((x, y))
@@ -68,9 +68,9 @@ class Fencer(pygame.sprite.Sprite):
         pressed_keys = pygame.key.get_pressed()
                 
         if pressed_keys[K_LEFT]:
-            self.acc.x = -ACC
+            self.acc.x = -ACC - 2
         if pressed_keys[K_RIGHT]:
-            self.acc.x = ACC          
+            self.acc.x = ACC + 2
             
         self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
@@ -88,7 +88,7 @@ class Fencer(pygame.sprite.Sprite):
         if hits:
             self.pos.y = hits[0].rect.top + 1
             self.vel.y = 0
-            
+
     def jump(self):
         hits = pygame.sprite.spritecollide(self, platforms, False)
         if hits:
@@ -108,14 +108,34 @@ class Floor(pygame.sprite.Sprite):
         self.img = self.images[0]
         self.rect = self.img.get_rect(center = (WIDTH/2, y))
 
-    
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((128,255,40))
+        self.rect = self.image.get_rect(center = (420, 350))
+
+        
+    def update(self):
+        hits = pygame.sprite.collide_mask(Box1, Fencer1)
+        if hits:
+            self.image.fill((255,0,0))
+            print("Hit")
+        else:
+            self.image.fill((128,255,40))
+            
 #170,730
 Fencer1 = Fencer(170,700)
 Floor1 = Floor(-50,730)
+Box1 = Box()
 
 all_sprites = pygame.sprite.Group()
+#all_sprites.add(Box1)
 all_sprites.add(Fencer1)
-#all_sprites.add(Floor1)
+
+boxes = pygame.sprite.Group()
+boxes.add(Box1)
 
 platforms = pygame.sprite.Group()
 platforms.add(Floor1)
@@ -133,14 +153,18 @@ while True:
     WIN.blit(bg_img, (0,0))
     #WIN.blit(base_img, (0,FLOOR))
     
+    for entity in boxes:
+        WIN.blit(entity.image, entity.rect)
+    
     for entity in platforms:
         WIN.blit(entity.img, entity.rect)
         
     for entity in all_sprites:
-        WIN.blit(entity.img, entity.rect)
+        WIN.blit(entity.image, entity.rect)
         
 
     pygame.display.update()
     FramePerSec.tick(FPS)
     Fencer1.move()
     Fencer1.update()
+    Box1.update()
